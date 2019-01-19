120328dbff89bc1d98e25813d33b3b4f5a8658ba
Jake Attard <jakeattard18@gmail.com>
2019-01-19 11:30:37 +1000

    YoutubeClone Project Setup

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Youtube Clone</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+    <div id="pageContainer">
+        <div id="mastHeadContainer">
+            
+        </div>
+
+        <div id="sideNavContainer">
+
+        </div>
+
+        <div id="mainSectionContainer">
+
+        </div>
+
+    </div>
+</body>
+</html>
