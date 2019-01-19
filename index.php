90675507266fc1b7a488da3d53c434ecb55d8b3d
Jake Attard <jakeattard18@gmail.com>
2019-01-19 15:26:13 +1000

    JS File Created

--- index.php
+++ index.php
@@ -3,12 +3,20 @@
 <head>
     <title>Youtube Clone</title>
 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
+    <script src="assets/js/commonActions.js"></script>
+
 </head>
 <body>
     <div id="pageContainer">
         <div id="mastHeadContainer">
-            Hello
+            <button class="navShowHide">Click me!</button>
         </div>
 
         <div id="sideNavContainer">
