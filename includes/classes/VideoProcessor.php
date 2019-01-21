b87f5c8ad6fc4a08e29cadf14d2e9314e7f8805a
Jake Attard <jakeattard18@gmail.com>
2019-01-21 12:50:48 +1000

    Upload Function Getting Called

---
+++ includes/classes/VideoProcessor.php
@@ -0,0 +1,14 @@
+<?php 
+class VideoProcessor {
+
+    private $con;
+
+    public function __construct($con) {
+        $this->con = $con;
+    }
+
+    public function upload($videoUploadData) {
+        
+    }
+}
+?>
--- processing.php
+++ processing.php
@@ -1,5 +1,7 @@
 <?php 
 require_once("includes/header.php"); 
+require_once("includes/classes/VideoUploadData.php");
+require_once("includes/classes/VideoProcessor.php");
 
 if(!isset($_POST['uploadButton'])) {
     echo "No file sent to page.";
@@ -7,4 +9,7 @@ if(!isset($_POST['uploadButton'])) {
 }
 
 $videoUploadData = new VideoUploadData($_POST["fileInput"], $_POST["titleInput"], $_POST["descriptionInput"], $_POST["privacyInput"], $_POST["categoryInput"], "REPLACE-THIS");
+
+$videoProcessor = new VideoProcessor($con);
+$wasSuccessful = $videoProcessor->upload($videoUploadData);
 ?>
