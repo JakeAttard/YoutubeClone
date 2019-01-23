15c71c6fec25e03bdc2a303168237709941efd02
Jake Attard <jakeattard18@gmail.com>
2019-01-23 22:33:43 +1000

    Styling Register Page

--- signIn.php
+++ signIn.php
@@ -6,6 +6,7 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/register.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
@@ -17,7 +18,9 @@
     <div class="signInContainer">
         <div class="column">
             <div class="header">
-            
+                <img src="assets/images/icons/VideoTubeLogo.png" title="logo" alt="Site Logo">
+                <h3>Sign Up</h3>
+                <span>to continue to Youtube Clone</span>
             </div>
             <div class="loginForm">
             
