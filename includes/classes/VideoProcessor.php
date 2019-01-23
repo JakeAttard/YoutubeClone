4ca647fbfc7d97a5d6f9f3be9daf1b50e7092b47
Jake Attard <jakeattard18@gmail.com>
2019-01-23 12:32:26 +1000

    Thumbnail Bug

--- includes/classes/VideoProcessor.php
+++ includes/classes/VideoProcessor.php
@@ -49,6 +49,8 @@ class VideoProcessor {
                 echo "Upload failed - could not generate thumbnails\n";
                 return false;
             }
+
+            return true;
         }
     }
 
@@ -132,14 +134,46 @@ class VideoProcessor {
 
         $videoId = $this->con->lastInsertId();
         $this->updateDuration($duration, $videoId);
+
+        for($num = 1; $num <= $numThumbnails; $num++) {
+            $imageName = uniqid() . ".jpg";
+            $interval = ($duration * 0.8) / $numThumbnails * $num;
+            $fullThumbnailPath = "$pathToThumbnail/$videoId-$imageName";
+
+            $cmd = "$this->ffmpegPath -i $filePath -ss $interval -s $thumbnailSize -vframes 1 $fullThumbnailPath 2>&1";
+
+            $outputLog = array();
+            exec($cmd, $outputLog, $returnCode);
+
+            if($returnCode != 0) {
+                foreach($outputLog as $line) {
+                    echo $line . "<br>";
+                }
+            }
+
+            $query = $this->con->prepare("INSERT INTO thumbnails(videoId, filePath, selected) VALUES(:videoId, :filePath, :selected)");
+            $query->bindParam(":videoId", $videoId);
+            $query->bindParam(":filePath", $fullThumbnailPath);
+            $query->bindParam(":selected", $selected);
+
+            $selected = $num == 1 ? 1 : 0;
+
+            $success = $query->execute();
+
+            if(!$success) {
+                echo "Error inserting thumbnail\n";
+                return false;
+            }
+        }
+
+        return true;
     }
 
     private function getVideoDuration($filePath) {
-        return shell_exec("$this->ffprobePath -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $filePath");
+        return (int)shell_exec("$this->ffprobePath -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $filePath");
     }
 
     private function updateDuration($duration, $videoId) {
-        $duration = (int)$duration;
         $hours = floor($duration / 3600);
         $mins = floor(($duration - ($hours * 3600)) / 60);
         $secs = floor($duration % 60);
