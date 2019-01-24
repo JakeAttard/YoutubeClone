1fe7fcba174b1f447c01791f096efdb2949379c5
Jake Attard <jakeattard18@gmail.com>
2019-01-24 20:27:37 +1000

    Validation of functions started

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -2,13 +2,20 @@
 class Account {
 
     private $con;
+    private $errorArray = array();
 
     public function __construct($con) {
         $this->con = $con;
     }
 
     public function register($firstName, $lastName, $username, $email, $confirmEmail, $password, $confirmPassword) {
-        
+        $this->validateFirstName($firstName);
+    }
+
+    private function validateFirstName($firstName) {
+        if(strlen($firstName) > 25 || strlen($firstName) < 2) {
+            array_push($this->errorArray, Constants::$firstNameCharacters);
+        }
     }
 }
 ?>
