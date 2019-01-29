e5213bcaacd0770d0e64c32f0fddba5d33dadd62
Jake Attard <jakeattard18@gmail.com>
2019-01-29 14:19:20 +1000

    ajax calls created

---
+++ assets/js/videoPlayerActions.js
@@ -0,0 +1,5 @@
+function likeVideo(button, videoId) {
+    $.post("ajax/likeVideo.php").done(function(data) {
+        alert(data);
+    });
+}
--- watch.php
+++ watch.php
@@ -11,6 +11,7 @@ if(!isset($_GET["id"])) {
 $video = new Video($con, $_GET["id"], $userLoggedInObj);
 $video->incrementViews();
 ?>
+<script src="assets/js/videoPlayerActions.js"></script>
 
 <div class="watchLeftColumn">
     <?php
