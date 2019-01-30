5467075c66d2a609b4d7dd01d6b0561631082254
Jake Attard <jakeattard18@gmail.com>
2019-01-30 17:43:53 +1000

    User Profile Button created

--- includes/classes/VideoInfoSection.php
+++ includes/classes/VideoInfoSection.php
@@ -26,7 +26,7 @@ class VideoInfoSection {
                     <h1>$title</h1>
 
                     <div class='bottomSection'>
-                        <span class='viewCount'>$views</span>
+                        <span class='viewCount'>$views views</span>
                         $controls
                     </div>
                 </div>";
@@ -34,6 +34,16 @@ class VideoInfoSection {
 
     private function createSecondaryInfo() {
 
+        $description = $this->video->getDescription();
+        $uploadDate = $this->video->getUploadDate();
+        $uploadedBy = $this->video->getUploadedBy();
+        $profileButton = ButtonProvider::createUserProfileButton($this->con, $uploadedBy);
+
+        return "<div class='secondaryInfo'>
+                    <div class='topRow'>
+                        $profileButton
+                    </div>
+                </div>";
     }
 }
 ?>
