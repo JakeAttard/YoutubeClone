51b3514b3af9fdad803e9000aa9ba03129db571b
Jake Attard <jakeattard18@gmail.com>
2019-01-31 00:19:17 +1000

    Subscribe button text updating

--- assets/js/userActions.js
+++ assets/js/userActions.js
@@ -5,7 +5,15 @@ function subscribe(userTo, userFrom, button) {
         return;
     }
 
-    $.post("ajax/subscribe.php", { userTo: userTo, userFrom: userFrom }).done(function(data) {
-        console.log(data);
+    $.post("ajax/subscribe.php", { userTo: userTo, userFrom: userFrom }).done(function(count) {
+
+        if(count != null) {
+            $(button).toggleClass("subscribe unsubscribe");
+
+            var buttonText = $(button).hasClass("subscribe") ? "SUBSCRIBE" : "SUBSCRIBED";
+            $(button).text(buttonText + " " + count);
+        } else {
+            alert("Something went wrong");
+        }
     });
 }
