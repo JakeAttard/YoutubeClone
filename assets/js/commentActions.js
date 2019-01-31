02745163ebccbb8d248ee81bae4869c1fc0710be
Jake Attard <jakeattard18@gmail.com>
2019-01-31 12:40:42 +1000

    Post comment function created

---
+++ assets/js/commentActions.js
@@ -0,0 +1,11 @@
+function postComment(button, postedBy, videoId, replyTo, containerClass) {
+    var textarea = $(button).siblings("textarea");
+    var commentText = textarea.val();
+    textarea.val("");
+
+    if(commentText) {
+        
+    } else {
+        alert("You can't post an empty comment");
+    }
+}
