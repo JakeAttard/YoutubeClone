06905f5fc38cc6a823f670d720041132dfeaeb37
Jake Attard <jakeattard18@gmail.com>
2019-01-31 23:16:28 +1000

    Comment bug when inserting to DB

--- ajax/postComment.php
+++ ajax/postComment.php
@@ -2,7 +2,22 @@
 require_once("../includes/config.php");
 
 if(isset($_POST['commentText']) && isset($_POST['postedBy']) && isset($_POST['videoId'])) {
-    echo "success";
+
+    $query = $con->prepare("INSERT INTO comments(postedBy, videoId, responseTo, body) VALUES(:postedBy, :videoId, :responseTo, :body)");
+    $query->bindParam(":postedby", $postedBy);
+    $query->bindParam(":videoId", $videoId);
+    $query->bindParam(":responseTo", $responseTo);
+    $query->bindParam(":body", $commentText);
+
+    $postedBy = $_POST['postedBy'];
+    $videoId = $_POST['videoId'];
+    $responseTo = $_POST['responseTo'];
+    $commentText = $_POST['commentText'];
+
+    $query->execute();
+
+    // Return new comment HTML
+
 } else {
     echo "One or more parameters are not passed into subscribe.php file";
 }
