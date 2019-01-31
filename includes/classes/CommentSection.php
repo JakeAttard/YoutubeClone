53d571a1bdf3d40186abb4e62489e1c9c42652cd
Jake Attard <jakeattard18@gmail.com>
2019-01-31 12:21:05 +1000

    Comment Form created

--- includes/classes/CommentSection.php
+++ includes/classes/CommentSection.php
@@ -22,6 +22,24 @@ class CommentSection {
         $profileButton = ButtonProvider::createUserProfileButton($this->con, $postedBy);
         $commentAction = "postComment(this, \"$postedBy\", $videoId, null, \"comments\")";
         $commentButton = ButtonProvider::createButton("COMMENT", null, $commentAction, "postComment");
+
+        // Get comment HTML
+
+        return "<div class='commentSection'>
+                    <div class='header'>
+                        <span class='commentCount'>$numComments Comments</span>
+
+                        <div class='commentForm'>
+                            $profileButton
+                            <textarea class='commentBodyClass' placeholder='Add a public comment'></textarea>
+                            $commentButton
+                        </div>
+                    </div>
+
+                    <div class='comments'>
+                        
+                    </div>
+                </div>";
     }
     
 }
