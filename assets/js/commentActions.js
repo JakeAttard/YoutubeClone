abd90a6e811f32af5c4d01bdda42cd16479bff3e
Jake Attard <jakeattard18@gmail.com>
2019-02-02 21:00:47 +1000

    Like comment ajax response

--- assets/js/commentActions.js
+++ assets/js/commentActions.js
@@ -20,7 +20,7 @@ function toggleReply(button) {
 }
 
 function likeComment(commentId, button, videoId) {
-    $.post("ajax/likeComment.php", { commentId: commentId, videoId: videoId }).done(function(data) {
+    $.post("ajax/likeComment.php", { commentId: commentId, videoId: videoId }).done(function(numToChange) {
         
         var likeButton = $(button);
         var dislikeButton = $(button).siblings(".dislikeButton");
@@ -28,11 +28,10 @@ function likeComment(commentId, button, videoId) {
         likeButton.addClass("active");
         dislikeButton.removeClass("active");
 
-        var result = JSON.parse(data);
-        updateLikesValue(likeButton.find(".text"), result.likes);
-        updateLikesValue(dislikeButton.find(".text"), result.dislikes);
+        var likesCount = $(button).siblings(".likesCount");
+        updateLikesValue(likesCount, numToChange);
 
-        if(result.likes < 0) {
+        if(numToChange < 0) {
             likeButton.removeClass("active");
             likeButton.find("img:first").attr("src", "assets/images/icons/thumb-up.png");
         } else {
@@ -45,4 +44,9 @@ function likeComment(commentId, button, videoId) {
 
 function dislikeComment(commentId, button, videoId) {
 
+}
+
+function updateLikesValue(element, num) {
+    var likesCountVal = element.text() || 0;
+    element.text(parseInt(likesCountVal) + parseInt(num));
 }
