949e3a7797ccf0e0dbe3d0768d862ff85afede2a
Jake Attard <jakeattard18@gmail.com>
2019-02-02 20:26:06 +1000

    likeComment and dislikeComment ajax request files created

--- assets/js/commentActions.js
+++ assets/js/commentActions.js
@@ -18,3 +18,31 @@ function toggleReply(button) {
 
     commentForm.toggleClass("hidden");
 }
+
+function likeComment(commentId, button, videoId) {
+    $.post("ajax/likeComment.php", { commentId: commentId, videoId: videoId }).done(function(data) {
+        
+        var likeButton = $(button);
+        var dislikeButton = $(button).siblings(".dislikeButton");
+
+        likeButton.addClass("active");
+        dislikeButton.removeClass("active");
+
+        var result = JSON.parse(data);
+        updateLikesValue(likeButton.find(".text"), result.likes);
+        updateLikesValue(dislikeButton.find(".text"), result.dislikes);
+
+        if(result.likes < 0) {
+            likeButton.removeClass("active");
+            likeButton.find("img:first").attr("src", "assets/images/icons/thumb-up.png");
+        } else {
+            likeButton.find("img:first").attr("src", "assets/images/icons/thumb-up-active.png");
+        }
+
+        dislikeButton.find("img:first").attr("src", "assets/images/icons/thumb-down.png");
+    });
+}
+
+function dislikeComment(commentId, button, videoId) {
+
+}
