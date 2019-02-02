1b9348ed5be2e75a7ee342e95f1f06014903d8d8
Jake Attard <jakeattard18@gmail.com>
2019-02-02 21:04:00 +1000

    Disliking Comments working

--- assets/js/commentActions.js
+++ assets/js/commentActions.js
@@ -43,7 +43,26 @@ function likeComment(commentId, button, videoId) {
 }
 
 function dislikeComment(commentId, button, videoId) {
+    $.post("ajax/dislikeComment.php", { commentId: commentId, videoId: videoId }).done(function(numToChange) {
+        
+        var dislikeButton = $(button);
+        var likeButton = $(button).siblings(".likeButton");
 
+        dislikeButton.addClass("active");
+        likeButton.removeClass("active");
+
+        var likesCount = $(button).siblings(".likesCount");
+        updateLikesValue(likesCount, numToChange);
+
+        if(numToChange > 0) {
+            dislikeButton.removeClass("active");
+            dislikeButton.find("img:first").attr("src", "assets/images/icons/thumb-down.png");
+        } else {
+            dislikeButton.find("img:first").attr("src", "assets/images/icons/thumb-down-active.png");
+        }
+
+        likeButton.find("img:first").attr("src", "assets/images/icons/thumb-up.png");
+    });
 }
 
 function updateLikesValue(element, num) {
