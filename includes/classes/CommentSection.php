48fd53b64e8d3d9131fdf6da07dcdadf23019f15
Jake Attard <jakeattard18@gmail.com>
2019-02-03 14:03:31 +1000

    Existing Comments Showing

--- includes/classes/CommentSection.php
+++ includes/classes/CommentSection.php
@@ -23,7 +23,11 @@ class CommentSection {
         $commentAction = "postComment(this, \"$postedBy\", $videoId, null, \"comments\")";
         $commentButton = ButtonProvider::createButton("COMMENT", null, $commentAction, "postComment");
 
-        // Get comment HTML
+        $comments = $this->video->getComments();
+        $commentItems = "";
+        foreach($comments as $comment) {
+            $commentItems .= $comment->create();
+        }
 
         return "<div class='commentSection'>
                     <div class='header'>
@@ -37,7 +41,7 @@ class CommentSection {
                     </div>
 
                     <div class='comments'>
-                        
+                        $commentItems
                     </div>
                 </div>";
     }
