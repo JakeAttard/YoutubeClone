fd2f8abcc7af351d34e10b8b70d4840d02694046
Jake Attard <jakeattard18@gmail.com>
2019-02-04 20:34:24 +1000

    Creating Subscriber Videos

--- includes/classes/SubscriptionsProvider.php
+++ includes/classes/SubscriptionsProvider.php
@@ -13,7 +13,36 @@ class SubscriptionsProvider {
         $subscriptions = $this->userLoggedInObj->getSubscriptions();
 
         if(sizeof($subscriptions) > 0) {
-            
+            $condition = "";
+            $i = 0;
+
+            while($i < sizeof($subscriptions)) {
+
+                if($i == 0) {
+                    $condition .= "WHERE uploadedBy=?";
+                } else {
+                    $condition .= " OR uploadedBy=?";
+                }
+                $i++;
+            }
+
+            $videoSql = "SELECT * FROM videos $condition ORDER BY uploadDate DESC";
+            $videoQuery = $this->con->prepare($videoQuery);
+
+            $i = 1;
+
+            foreach($subscriptions as $sub) {
+                $videoQuery->bindParam($i, $subUsername);
+                $subUsername = $sub->getUsername();
+                $i++;
+            }
+
+            $videoQuery->execute();
+
+            while($row = $videoQuery->fetch(PDO::FETCH_ASSOC)) {
+                $video = new Video($this->con, $row, $this->userLoggedInObj);
+                array_push($videos, $video);
+            }
         }
 
         return $videos;
