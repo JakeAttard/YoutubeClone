bc909c2e297eae62bceae8b4b2a97b968f0b854d
Jake Attard <jakeattard18@gmail.com>
2019-02-04 20:20:49 +1000

    SubscriptionsProvider Class Created

---
+++ includes/classes/SubscriptionsProvider.php
@@ -0,0 +1,22 @@
+<?php
+class SubscriptionsProvider {
+
+    private $con, $userLoggedInObj;
+
+    public function __construct($con, $userLoggedInObj) {
+        $this->con = $con;
+        $this->userLoggedInObj = $userLoggedInObj;
+    }
+
+    public function getVideos() {
+        $videos = array();
+        $subscriptions = $this->userLoggedInObj->getSubscriptions();
+
+        if(sizeof($subscriptions) > 0) {
+            
+        }
+
+        return $videos;
+    }
+}
+?>
--- index.php
+++ index.php
@@ -2,7 +2,15 @@
 
 <div class="videoSection">
     <?php
+    $subscriptionsProvider = new SubscriptionsProvider($con, $userLoggedInObj);
+    $subscriptionVideos = $subscriptionsProvider->getVideos();
+
     $videoGrid = new VideoGrid($con, $userLoggedInObj->getUsername());
+
+    if(User::isLoggedIn() && sizeOf($subscriptionVideos) > 0) {
+        echo $videoGrid->create($subscriptionVideos, "Subscriptions", false);
+    }
+
     echo $videoGrid->create(null, "Recommended", false);
     ?>
 </div>
