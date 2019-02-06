5cd67906e2d0ab2bf9b7742bcaea0b90bc103ddd
Jake Attard <jakeattard18@gmail.com>
2019-02-06 16:09:51 +1000

    Trending Videos Page Complete

--- trending.php
+++ trending.php
@@ -1,5 +1,19 @@
 <?php
 require_once("includes/header.php");
+require_once("includes/classes/TrendingProvider.php");
 
+$trendingProvider = new TrendingProvider($con, $userLoggedInObj);
+$videos = $trendingProvider->getVideos();
 
+$videoGrid = new VideoGrid($con, $userLoggedInObj);
 ?>
+
+<div class="largeVideoGridContainer">
+    <?php 
+    if(sizeOf($videos) > 0) {
+        echo $videoGrid->createLarge($video, "Trending videos uploaded in the last week", false);
+    } else {
+        echo "No trending videos to show";
+    }
+    ?>
+</div>
