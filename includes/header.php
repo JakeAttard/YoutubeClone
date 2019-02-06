4e54e73c847770c0eef5451cad3111265e05bc53
Jake Attard <jakeattard18@gmail.com>
2019-02-06 11:47:52 +1000

    Outputting Navigation Bar Links

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,7 @@ require_once("includes/classes/Video.php");
 require_once("includes/classes/VideoGrid.php"); 
 require_once("includes/classes/VideoGridItem.php"); 
 require_once("includes/classes/SubscriptionsProvider.php");
+require_once("includes/classes/NavigationMenuProvider.php");
 
 $usernameLoggedIn = User::isLoggedIn() ? $_SESSION["userLoggedIn"] : "";
 $userLoggedInObj = new User($con, $usernameLoggedIn);
@@ -56,7 +57,10 @@ $userLoggedInObj = new User($con, $usernameLoggedIn);
         </div>
 
         <div id="sideNavContainer" style="display:none;">
-
+            <?php 
+            $navigationProvider = new NavigationMenuProvider($con, $userLoggedInObj);
+            echo $navigationProvider->create();
+            ?>
         </div>
 
         <div id="mainSectionContainer">
