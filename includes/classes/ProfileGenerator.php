ce8975655e4e2ede2a049657929b5556cdd22e23
Jake Attard <jakeattard18@gmail.com>
2019-02-06 17:48:23 +1000

    ProfileData Class Created

--- includes/classes/ProfileGenerator.php
+++ includes/classes/ProfileGenerator.php
@@ -1,16 +1,19 @@
 <?php
+require_once("ProfileData.php");
+
 class ProfileGenerator {
 
-    private $con, $userLoggedInObj, $profileUsername;
+    private $con, $userLoggedInObj, $profileData;
 
     public function __construct($con, $userLoggedInObj, $profileUsername) {
         $this->con = $con;
         $this->userLoggedInObj = $userLoggedInObj;
-        $this->profileUsername = $profileUsername;
+        $this->profileData = new ProfileData($con, $profileUsername);
     }
 
     public function create() {
-        
+        $profileUsername = $this->profileData->getProfileUsername();
+        echo $profileUsername;
     }
 }
 ?>
