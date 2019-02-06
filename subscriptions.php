db8405a3aab1cdc8d9db6d0cd1c81f09fa0551b0
Jake Attard <jakeattard18@gmail.com>
2019-02-06 16:12:58 +1000

    Subscriptions Page Complete

---
+++ subscriptions.php
@@ -0,0 +1,18 @@
+<?php
+require_once("includes/header.php");
+
+$subscriptionsProvider = new SubscriptionsProvider($con, $userLoggedInObj);
+$videos = $subscriptionsProvider->getVideos();
+
+$videoGrid = new VideoGrid($con, $userLoggedInObj);
+?>
+
+<div class="largeVideoGridContainer">
+    <?php 
+    if(sizeOf($videos) > 0) {
+        echo $videoGrid->createLarge($video, "New vidoes from your subscribers", false);
+    } else {
+        echo "No new subscribers videos to show";
+    }
+    ?>
+</div>
