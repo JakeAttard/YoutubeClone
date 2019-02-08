2b58e197a2232ae4585da2d1f284fa67dbf5dee2
Jake Attard <jakeattard18@gmail.com>
2019-02-08 12:05:26 +1000

    User Details Form Created

--- settings.php
+++ settings.php
@@ -2,15 +2,20 @@
 require_once("includes/header.php");
 require_once("includes/classes/Account.php");
 require_once("includes/classes/FormSanitizer.php");
+require_once("includes/classes/SettingsFormProvider.php");
 require_once("includes/classes/Constants.php");
 
 if(!User::isLoggedIn()) {
     header("Location: signIn.php");
 }
+
+$formProvider = new SettingsFormProvider();
 ?>
 
 <div class="settingsContainer column">
     <div class="formSection">
-
+        <?php
+        echo $formProvider->createUserDetailsForm();
+        ?>
     </div>
 </div>
