b387234e4cdd4ef831ff8d5730bb9ed59dab36bd
Jake Attard <jakeattard18@gmail.com>
2019-02-23 13:26:32 +1000

    Changes / Fixes

--- includes/config.php
+++ includes/config.php
@@ -5,7 +5,7 @@ session_start();
 date_default_timezone_set("Australia/Brisbane");
 
 try {
-    $con = new PDO("mysql:dbname=youtubeclone;host=localhost", "root", "root");
+    $con = new PDO("mysql:dbname=youtubeclone;host=localhost", "root", "");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 } 
 catch (PDOException $e) {
--- signUp.php
+++ signUp.php
@@ -41,6 +41,7 @@ function getInputValue($name) {
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/register.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
